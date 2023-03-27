cab835354ffd97c1b312641f47bd1c208cfc1d19
Arcobale <1144209486@qq.com>
2023-03-27 17:00:12 +0800

    产品小图列表动态展示

--- src/pages/Detail/ImageList/ImageList.vue
+++ src/pages/Detail/ImageList/ImageList.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="swiper-container">
     <div class="swiper-wrapper">
-      <div class="swiper-slide">
-        <img src="../images/s1.png">
+      <div class="swiper-slide" v-for="(image, index) in skuImageList" :key="image.id">
+        <img :src="image.imgUrl">
       </div>
     </div>
     <div class="swiper-button-next"></div>
@@ -15,6 +15,7 @@
   import Swiper from 'swiper'
   export default {
     name: "ImageList",
+    props: ['skuImageList'],
   }
 </script>
 
