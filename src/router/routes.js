822824b0714c682412fb9a3647c6ca2173752e7a
Arcobale <1144209486@qq.com>
2023-03-28 22:44:51 +0800

    购物车静态组件添加

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,8 +5,14 @@ import Login from '@/pages/Login'
 import Register from '@/pages/Register'
 import Detail from '@/pages/Detail'
 import AddCartSuccess from '@/pages/AddCartSuccess'
+import ShopCart from '@/pages/ShopCart'
 
 export default [
+    {
+        path: "/shopcart",
+        component: ShopCart,
+        meta: { show: true },
+    },
     {
         path: "/addcartsuccess",
         component: AddCartSuccess,
