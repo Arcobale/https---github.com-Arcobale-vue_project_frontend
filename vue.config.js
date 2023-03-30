970942f203b805ebd4a3cb90e53fe5f7cf750ce6
Arcobale <1144209486@qq.com>
2023-03-30 23:43:39 +0800

    处理map文件

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  //关闭项目打包时产生map
+  productionSourceMap: false,
   transpileDependencies: true,
   //关闭eslint
   lintOnSave: false,
